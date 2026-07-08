ece92eeef3fcb63559cb95bab57095f6c2c5a773
MonsterSnack <smitdamian13@gmail.com>
2026-07-08 20:03:23 +0200

    Bump version for analytics graphs

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 const FlipTrackerProConfig = Object.freeze({
   appName: 'Flip Tracker Pro',
   shortName: 'FTP',
-  version: '0.3.2',
+  version: '0.4.0',
   rootId: 'flip-tracker-pro-root',
   storagePrefix: 'flipTrackerPro',
   defaultWindow: {
