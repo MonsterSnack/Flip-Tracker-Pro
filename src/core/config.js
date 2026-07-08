c202d4138904e2b2eff17d877e6071bfe2e3ccbe
MonsterSnack <smitdamian13@gmail.com>
2026-07-08 18:22:46 +0200

    chore(core): bump version for edit saved flips

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 const FlipTrackerProConfig = Object.freeze({
   appName: 'Flip Tracker Pro',
   shortName: 'FTP',
-  version: '0.1.9',
+  version: '0.2.0',
   rootId: 'flip-tracker-pro-root',
   storagePrefix: 'flipTrackerPro',
   defaultWindow: {
