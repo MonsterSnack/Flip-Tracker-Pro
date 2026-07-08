8b85ab8e4f0e9ba62e4cd3db2fdd55a378505bd6
MonsterSnack <smitdamian13@gmail.com>
2026-07-08 17:46:35 +0200

    chore(core): bump version for minimize fix

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 const FlipTrackerProConfig = Object.freeze({
   appName: 'Flip Tracker Pro',
   shortName: 'FTP',
-  version: '0.1.2',
+  version: '0.1.3',
   rootId: 'flip-tracker-pro-root',
   storagePrefix: 'flipTrackerPro',
   defaultWindow: {
