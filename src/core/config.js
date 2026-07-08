85879636ea81dc09095d698c1b3ce75b561662aa
MonsterSnack <smitdamian13@gmail.com>
2026-07-08 18:31:48 +0200

    Bump version for saved flip filters

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 const FlipTrackerProConfig = Object.freeze({
   appName: 'Flip Tracker Pro',
   shortName: 'FTP',
-  version: '0.2.0',
+  version: '0.2.1',
   rootId: 'flip-tracker-pro-root',
   storagePrefix: 'flipTrackerPro',
   defaultWindow: {
