38c7deb58226ae9e1a24cc0471d662d703bef3d8
MonsterSnack <smitdamian13@gmail.com>
2026-07-08 19:55:20 +0200

    Bump version for saved flip notes

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 const FlipTrackerProConfig = Object.freeze({
   appName: 'Flip Tracker Pro',
   shortName: 'FTP',
-  version: '0.3.1',
+  version: '0.3.2',
   rootId: 'flip-tracker-pro-root',
   storagePrefix: 'flipTrackerPro',
   defaultWindow: {
