aba1fc39a7aa54ef6cdecba6e19b73716070a42a
MonsterSnack <smitdamian13@gmail.com>
2026-07-09 13:37:46 +0200

    Bump config version to 0.7.3

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 const FlipTrackerProConfig = Object.freeze({
   appName: 'Flip Tracker Pro',
   shortName: 'FT',
-  version: '0.7.2',
+  version: '0.7.3',
   rootId: 'flip-tracker-pro-root',
   storagePrefix: 'flipTrackerPro',
   defaultWindow: {
